dbc913b4fc5e58ec762670a1c90dd489430b21e2
VladOlhovsk <olhovskijvladislav27@gmail.com>
2024-10-30 19:00:34 +0200

    fix2

--- Lesson_32_3_C++/Toy.cpp
+++ Lesson_32_3_C++/Toy.cpp
@@ -1,27 +1,27 @@
 #include "Toy.h"
 
-fun::Toy::Toy()
+products::fun::Toy::Toy()
 {
     material = "none";
 }
 
-fun::Toy::Toy(std::string name, float price, std::string manufacture, int age, std::string material):
+products::fun::Toy::Toy(std::string name, float price, std::string manufacture, int age, std::string material):
     products::Product(name, price, manufacture, age)
 {
     this->material = material;
 }
 
-void fun::Toy::setMaterial(std::string material)
+void products::fun::Toy::setMaterial(std::string material)
 {
     this->material = material;
 }
 
-std::string fun::Toy::getMaterial() const
+std::string products::fun::Toy::getMaterial() const
 {
     return material;
 }
 
-void fun::Toy::show() const
+void products::fun::Toy::show() const
 {
     cout << "Toy: " << endl;
     cout << "Name: " << name << "\nPrice: " << price << endl
@@ -31,7 +31,7 @@ void fun::Toy::show() const
 
 }
 
-float fun::Toy::calcPrice() const
+float products::fun::Toy::calcPrice() const
 {
     float total = price;
     if (material.find("nature") != -1) 
