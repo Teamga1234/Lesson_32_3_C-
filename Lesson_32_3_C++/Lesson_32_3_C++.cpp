4ee10cd82a8f91d56d79c9dddbce7f13a00eb396
VladOlhovsk <olhovskijvladislav27@gmail.com>
2024-10-30 19:06:22 +0200

    test1

--- Lesson_32_3_C++/Lesson_32_3_C++.cpp
+++ Lesson_32_3_C++/Lesson_32_3_C++.cpp
@@ -5,5 +5,11 @@
 int main()
 {
 	std::vector < products::Product*> Lotery;
-	Lotery.push_back(new products::)
+	Lotery.push_back(new products::fun::Toy("Ball", 100, "Chine",
+		34, "natural leather"));
+	Lotery.push_back(new products::sport::SportEquipment("Bike",
+		10200, "Ukraine", 7, 12, "cycling"));
+	int user = rand() % Lotery.size();
+	Lotery[user]->show();
+	cout << "Equvivalent: " << Lotery[user]->calcPrice();
 }
