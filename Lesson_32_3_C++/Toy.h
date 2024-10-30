acc64961987bea5bca10123491e7fc3cbdfb92e1
VladOlhovsk <olhovskijvladislav27@gmail.com>
2024-10-30 18:36:37 +0200

    toy h

---
+++ Lesson_32_3_C++/Toy.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Product.h"
+
+namespace fun 
+{
+	class Toy : public products::Product
+	{
+	protected:
+		std::string material;
+	public:
+		Toy();
+		Toy(std::string name, float price, std::string manufacture,
+			int age, std::string material);
+
+		void setMaterial(std::string material);
+		std::string getMaterial()const;
+		void show()const override;
+		float calcPrice()const override;
+	};
+}
+
+
