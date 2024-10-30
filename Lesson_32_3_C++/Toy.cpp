7b69aac397de39cdaff3a7a971cd1aa353557b7d
VladOlhovsk <olhovskijvladislav27@gmail.com>
2024-10-30 18:39:14 +0200

    toy cpp

--- Lesson_32_3_C++/Toy.cpp
+++ Lesson_32_3_C++/Toy.cpp
@@ -1,1 +1,31 @@
 #include "Toy.h"
+
+fun::Toy::Toy()
+{
+    material = "none";
+}
+
+fun::Toy::Toy(std::string name, float price, std::string manufacture, int age, std::string material):
+    products::Product(name, price, manufacture, age)
+{
+    this->material = material;
+}
+
+void fun::Toy::setMaterial(std::string material)
+{
+    this->material = material;
+}
+
+std::string fun::Toy::getMaterial() const
+{
+    return material;
+}
+
+void fun::Toy::show() const
+{
+}
+
+float fun::Toy::calcPrice() const
+{
+    return 0.0f;
+}
